23f6ca5913063a293ed524f58b6cc03beda54d3a
Adam Stankiewicz <sheerun@sher.pl>
2014-08-26 07:14:48 +0200

    Require raven only in production

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,9 +1,11 @@
-require 'raven'
+if Rails.env.production?
+  require 'raven'
 
-Raven.configure do |config|
-  config.dsn = ENV['RAVEN_DSN']
-  config.environments = %w[ production ]
-  config.current_environment = Rails.env
-  config.tags = { environment: Rails.env }
-  config.excluded_exceptions = []
+  Raven.configure do |config|
+    config.dsn = ENV['RAVEN_DSN']
+    config.environments = %w[ production ]
+    config.current_environment = Rails.env
+    config.tags = { environment: Rails.env }
+    config.excluded_exceptions = []
+  end
 end

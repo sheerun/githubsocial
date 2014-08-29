ce2a0cc67c61880046334e8cb58283d7c0009f4f
Adam Stankiewicz <sheerun@sher.pl>
2014-08-29 16:29:06 +0200

    Memcache api response

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -9,19 +9,7 @@ class ApiController < ApplicationController
   end
 
   def related_repos
-    repo = Repo.find_by(name: params[:name], owner: params[:owner])
-    user_id = params[:user_id] ? params[:user_id].to_i : nil
-    penalize_factor = (1.5 ** [9, [1, params[:popularity].to_i].max].min).floor
-
-    if repo.present?
-      recommended = recommender.recommend(repo,
-        user_id: user_id,
-        penalize_factor: penalize_factor
-      )
-      render json: JSON.pretty_generate(recommended)
-    else
-      render json: []
-    end
+    render json: cached_related_repos
   end
 
   def not_found
@@ -31,6 +19,25 @@ class ApiController < ApplicationController
   end
 
   private
+
+  def cached_related_repos
+    Rails.cache.fetch(request.url, expires_in: 30.minutes) do
+      repo = Repo.find_by(name: params[:name], owner: params[:owner])
+      user_id = params[:user_id] ? params[:user_id].to_i : nil
+      penalize_factor = (1.5 ** [9, [1, params[:popularity].to_i].max].min).floor
+
+      if repo.present?
+        recommended = recommender.recommend(repo,
+          user_id: user_id,
+          penalize_factor: penalize_factor
+        )
+
+        JSON.pretty_generate(recommended)
+      else
+        JSON.pretty_generate([])
+      end
+    end
+  end
 
   def recommender
     RepoRecommenderCloud.instance

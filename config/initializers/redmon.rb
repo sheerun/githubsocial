c4d3d6904d9686eadac4c6831b9e4f1e7204b65c
Adam Stankiewicz <sheerun@sher.pl>
2014-08-29 13:27:00 +0200

    Move redmon initializer

--- config/initializers/redmon.rb
+++ config/initializers/redmon.rb
@@ -3,6 +3,6 @@ require 'redmon/redis'
 require 'redmon/app'
 
 Redmon.configure do |config|
-  config.redis_url = ENV['REDIS_URL'] || 'redis://127.0.0.1:6379'
+  config.redis_url = 'redis://127.0.0.1:6379'
   config.namespace = 'redmon'
 end

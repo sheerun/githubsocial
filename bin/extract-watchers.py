8fb586f1a1670eac84b26b3e91010168b729073d
Adam Stankiewicz <sheerun@sher.pl>
2014-10-20 13:52:12 +0200

    Add template extract scripts (github torren)

---
+++ bin/extract-watchers.py
@@ -0,0 +1,27 @@
+from bsonstream import KeyValueBSONInput
+import sys
+from sys import argv
+import gzip
+from bson.json_util import dumps
+
+f = sys.stdin
+stream = KeyValueBSONInput(fh=f, fast_string_prematch="github")
+for dict_data in stream:
+    # for k in dict_data.keys():
+    #     if k.endswith('url'):
+    #         dict_data.pop(k)
+
+    # dict_data.pop('_id')
+
+    # if 'permissions' in dict_data:
+    #     dict_data.pop('permissions')
+
+    # dict_data['owner_id'] = dict_data['owner']['id']
+    # dict_data.pop('owner')
+
+    # for field in ['organization', 'parent', 'source']:
+    #     if field in dict_data:
+    #         dict_data[field + '_id'] = dict_data[field]['id']
+    #         dict_data.pop(field)
+
+    print dumps(dict_data)
